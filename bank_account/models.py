6a07012d1ce62a161a7b0b1235f24dbd87a9f83d
Merit <meritehab@hotmail.com>
2018-01-20 18:38:36 +0200

    Add is_admin field to user model

--- bank_account/models.py
+++ bank_account/models.py
@@ -9,3 +9,4 @@ from django.db import models
 class BankAccountUser(AbstractUser):
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
     iban = models.CharField(max_length=35, verbose_name=("IBAN"))
+    is_admin = models.BooleanField(default=True)
