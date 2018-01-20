9707e905bdfe2c752da51f9e1a314b91682f59d5
Merit <meritehab@hotmail.com>
2018-01-20 02:38:32 +0200

    Change in models

--- bank_account/models.py
+++ bank_account/models.py
@@ -7,5 +7,5 @@ from django.db import models
 
 
 class BankAccountUser(AbstractUser):
-    creator = models.ForeignKey(settings.AUTH_USER_MODEL)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
     iban = models.CharField(max_length=35, verbose_name=("IBAN"))
